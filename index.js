94d97053efe79f7f4254578a6f829a2023283773
Ndoria <k.ndoriamurigu@gmail.com>
2024-07-13 14:52:13 +0300

    complete labs

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+
+
+const returnFirstTwoDrivers = function (nameArr) {
+
+    nameArr = ['Antonia',"Nuru",'Amari','Mo']
+
+    const newArr = nameArr.slice(0,2);
+    return newArr;
+}
+
+const returnLastTwoDrivers = function (nameArr) {
+    nameArr = ['Antonia',"Nuru",'Amari','Mo']
+
+    const newArr2 = nameArr.slice(-2);
+    return newArr2;
+}
+
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ]
+
+function createFareMultiplier(number) {
+    return function (fare) {
+       return fare * number;
+    }
+}
+
+const fareDoubler = (function(fare) {
+    return fare*2
+})
+
+const fareTripler = (function(fare) {
+    return fare*3
+})
+
+
+function selectDifferentDrivers(nameArr, selectingFunction) {
+    if (selectingFunction === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(nameArr);
+    } else if (selectingFunction === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(nameArr);
+    }
+}
